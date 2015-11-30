5bea6721bba68b75dbab25bb6100b097b5dafd17
lihe <liheemail@163.com>
2015-11-30 09:25:57 +0800

    add HelloWorld java file

---
+++ src/main/java/com/lihe/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lihe.test;
+
+/**
+ * Created by lihe on 15-11-30.
+ */
+public class HelloWorld {
+    public static void main() {
+        System.out.println("Hello World!");
+    }
+}
