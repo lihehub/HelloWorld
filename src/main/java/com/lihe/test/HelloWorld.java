cabe12b920b2bea30538a0eadf10925e4bcb6bbc
lihe <liheemail@163.com>
2015-11-30 13:14:10 +0800

    Hello World2

--- src/main/java/com/lihe/test/HelloWorld.java
+++ src/main/java/com/lihe/test/HelloWorld.java
@@ -5,6 +5,6 @@ package com.lihe.test;
  */
 public class HelloWorld {
     public static void main() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
